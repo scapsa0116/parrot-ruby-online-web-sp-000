60955ab1ab2965086dae9bbc676bc77319da1322
Stela Capsa <stela.capsa@gmail.com>
2019-11-09 22:21:26 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
-def parrot(phrase = "Squawk!")
+def parrot(phrase = "Pretty bird!)
   
 end
 parrot(phrase)
